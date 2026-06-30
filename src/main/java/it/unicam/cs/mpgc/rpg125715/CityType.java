08115487e1f10ef024c18fb3e16c92ccf2e3896b
mancio <alessandr.mancini@studenti.unicam.it>
2026-06-30 16:38:21 +0200

    Città previste dal gioco

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125715/CityType.java
@@ -0,0 +1,74 @@
+package it.unicam.cs.mpgc.rpg125715;
+
+public enum CityType {
+
+    //QIN SHI HUANG
+    Xianyang("Xianyang", LeaderType.QIN_SHI_HUANG, true),
+    Hong_Kong("Hong Kong", LeaderType.QIN_SHI_HUANG, false),
+    Delhi("Delhi", LeaderType.QIN_SHI_HUANG, false),
+    Tokyo("Tokyo", LeaderType.QIN_SHI_HUANG, false),
+
+    //ATTILA
+    Mosca("Mosca", LeaderType.ATTILA, true),
+    Astana("Astana", LeaderType.ATTILA, false),
+    Bratsk("Bratsk", LeaderType.ATTILA, false),
+    Stoccolma("Stoccolma", LeaderType.ATTILA, false),
+
+    //ALESSANDRO MAGNO
+    Pella("Pella", LeaderType.ALESSANDRO_MAGNO, true),
+    Creta("Creta", LeaderType.ALESSANDRO_MAGNO, false),
+    Ankara("Ankara", LeaderType.ALESSANDRO_MAGNO, false),
+    Babilonia("Babilonia", LeaderType.ALESSANDRO_MAGNO, false),
+    Gedda("Gedda", LeaderType.ALESSANDRO_MAGNO, false),
+
+    //GIULIO CESARE
+    Roma("Roma", LeaderType.GIULIO_CESARE, true),
+    Vienna("Vienna", LeaderType.GIULIO_CESARE, false),
+    Cagliari("Cagliari", LeaderType.GIULIO_CESARE, false),
+
+    //GIOVANNA D'ARCO
+    Parigi("Parigi", LeaderType.GIOVANNA_D_ARCO, true),
+    Marsiglia("Marsiglia", LeaderType.GIOVANNA_D_ARCO, false),
+    Amsterdam("Amsterdam", LeaderType.GIOVANNA_D_ARCO, false),
+    Rennes("Rennes", LeaderType.GIOVANNA_D_ARCO, false),
+
+    //ELISABETTA
+    Londra("Londra", LeaderType.REGINA_ELISABETTA, true),
+    Edimburgo("Edimburgo", LeaderType.REGINA_ELISABETTA, false),
+    Dublino("Dublino", LeaderType.REGINA_ELISABETTA, false),
+    Reykjavik("Reykjavik", LeaderType.REGINA_ELISABETTA, false),
+
+    //FEDERICO BARBAROSSA
+    Berlino("Berlino", LeaderType.FEDERICO_BARBAROSSA, true),
+    Varsavia("Varsavia", LeaderType.FEDERICO_BARBAROSSA, false),
+    Copenaghen("Copenaghen", LeaderType.FEDERICO_BARBAROSSA, false),
+
+    //ANNIBALE
+    Cartagine("Cartagine", LeaderType.ATTILA, true),
+    Il_Cairo("Il Cairo", LeaderType.ATTILA, false),
+    Rabat("Rabat", LeaderType.ATTILA, false),
+    Madrid("Madrid", LeaderType.ATTILA, false),
+    Tariff("Tariff", LeaderType.ATTILA, false);
+
+
+    private final String displayName;
+    private final LeaderType leaderType;
+    private final boolean capital;
+    CityType(String displayName, LeaderType leaderType, boolean capital) {
+        this.displayName = displayName;
+        this.leaderType = leaderType;
+        this.capital = capital;
+    }
+    //NOME CITTA
+    public String getDisplayName() {
+        return displayName;
+    }
+    //A CHE LEADER APPARTIENE
+    public LeaderType getLeaderType() {
+        return leaderType;
+    }
+    //TRUE SE CAPITALE
+    public boolean isCapital() {
+        return capital;
+    }
+}
