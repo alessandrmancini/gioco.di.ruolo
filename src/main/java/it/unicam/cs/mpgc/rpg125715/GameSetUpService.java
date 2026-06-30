04d29668d348a740ac7e7315898c1f970416b688
mancio <alessandr.mancini@studenti.unicam.it>
2026-06-30 12:16:24 +0200

    classe per inizializzazione partita

--- src/main/java/it/unicam/cs/mpgc/rpg125715/GameSetUpService.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125715/GameSetUpService.java
@@ -1,5 +1,59 @@
 package it.unicam.cs.mpgc.rpg125715;
 
 public class GameSetUpService {
-    
+        private final IdGenerator idGenerator;
+
+        public GameSetUpService(IdGenerator idGenerator) {
+            if(idGenerator == null){throw new IllegalArgumentException("idGenerator null");}
+            this.idGenerator = idGenerator;
+        }
+
+        public record StartingProfile(String territoryName, String capitalName){}
+
+        public StartingProfile getStartingProfile(LeaderType leader){
+                if(leader == null){throw new IllegalArgumentException("leader is null");}
+                return switch (leader){
+                    case GIULIO_CESARE -> new StartingProfile("Italia", "Roma");
+                    case ALESSANDRO_MAGNO -> new StartingProfile("Balcani", "Pella");
+                    case ANNIBALE -> new StartingProfile("Nord Africa", "Cartagine");
+                    case REGINA_ELISABETTA -> new StartingProfile("Inghilterra", "Londra");
+                    case ATTILA -> new StartingProfile("Urali", "Mosca");
+                    case GIOVANNA_D_ARCO -> new StartingProfile("Francia", "Parigi");
+                    case FEDERICO_BARBAROSSA -> new StartingProfile("Germania", "Berlino");
+                    case QIN_SHI_HUANG -> new StartingProfile("Cina", "Xianyang");
+                };
+        }
+
+        public Territory creaTerritorioPerLeader(LeaderType leader){
+            StartingProfile profile = getStartingProfile(leader);
+
+            return new Territory(idGenerator.nextId(EntityType.TERRITORY), profile.territoryName());
+        }
+
+        public City creaCapitaleyPerLeader(LeaderType leader, Player owner){
+            if(owner == null){throw new IllegalArgumentException("owner is null");}
+            StartingProfile profile = getStartingProfile(leader);
+            City capitale = new City(profile.capitalName());
+            capitale.setOwner(owner);
+            return capitale;
+        }
+
+        public Player creaPlayerIniziale(String nome, LeaderType leader, int denari){
+            if(nome == null || nome.isBlank()){throw new IllegalArgumentException("nome is null");}
+            if(leader == null){throw new IllegalArgumentException("leader is null");}
+
+            Territory territory = creaTerritorioPerLeader(leader);
+            Player player = new Player(idGenerator.nextId(EntityType.PLAYER),nome, leader, denari, territory);
+            City capitale = creaCapitaleyPerLeader(leader, player);
+
+            territory.addCity(capitale);
+            territory.setCapitale(capitale);
+
+            return player;
+        }
+
+        public Location creaLocation(int x, int y){
+            return new Location(idGenerator.nextId(EntityType.LOCATION), x, y);
+        }
+
 }
